1874ef2aa097cc29a66d836d5aae613c6fcfa018
Alex <nike95125@gmail.com>
2022-08-04 17:50:17 +0300

    added header & footer

--- gulpfile.js
+++ gulpfile.js
@@ -78,12 +78,6 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-    'node_modules/mixitup/dist/mixitup.min.js',
-    'app/js/jquery.rateyo.min.js',
     'app/js/main.js',
   ])
   .pipe(concat('main.min.js'))
@@ -127,4 +121,4 @@ exports.cleanDist = cleanDist;
 exports.dist = series(cleanDist, dist);
 
 
-exports.default = parallel(nunjucks, critGenerate, html, styles, scripts, img, browsersync, watching);
+exports.default = parallel(nunjucks, styles, scripts, img, browsersync, watching);
