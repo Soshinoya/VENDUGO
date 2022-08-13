4e5de9da0bf023d37cd49f5d68940a6a61c045ca
Alex <nike95125@gmail.com>
2022-08-13 21:36:15 +0300

    addded slider constructor

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,36 +1,74 @@
-// Variables
-const dotClass = 'main-slider__dot';
-const slideClass = 'main-slider__item';
-const slideArray = document.querySelectorAll(`.${slideClass}`);
-const dotsInner = document.querySelector('.main-slider__dots-inner');
-const DOTS_COUNT = slideArray.length;
+// Dot Slider
+class DotSlider {
 
-// Create Dots
-for (let i = 0; i < DOTS_COUNT; i++) {
-    const dot = document.createElement('li');
-    dot.classList.add(dotClass);
-    dot.setAttribute(dotClass, i);
-    dotsInner.append(dot);
+    constructor(obj = {}) {
+        this.dotClass = obj.dotClass;
+        this.slideClass = obj.slideClass;
+        this.dotsInnerClass = obj.dotsInnerClass;
+
+        this.slideArray = document.querySelectorAll(`.${this.slideClass}`);
+        this.dotsInner = document.querySelector(`.${this.dotsInnerClass}`);
+        this.DOTS_COUNT = this.slideArray.length;
+
+        this.createDots();
+    };
+
+    createDots() {
+        for (let i = 0; i < this.DOTS_COUNT; i++) {
+            const dot = document.createElement('li');
+            dot.classList.add(this.dotClass);
+            dot.setAttribute(this.dotClass, i);
+            this.dotsInner.append(dot);
+        };
+        this.dotsArray = document.querySelectorAll(`.${this.dotClass}`);
+        this.firstClassChange();
+    };
+
+    firstClassChange() {
+        this.dotsArray[0].classList.add(`${this.dotClass}--active`);
+        this.slideArray[0].classList.add(`${this.slideClass}--active`);
+        this.checkClick();
+    };
+
+    checkClick() {
+        for (const dot of this.dotsArray) {
+            dot.addEventListener('click', () => this.classChange(dot));
+        };
+    };
+
+    classChange(e) {
+        for (const dot of this.dotsArray) {
+            dot.classList.remove(`${this.dotClass}--active`);
+        };
+        for (const slide of this.slideArray) {
+            slide.classList.remove(`${this.slideClass}--active`);
+        };
+        e.classList.add(`${this.dotClass}--active`);
+        this.slideArray[e.getAttribute(this.dotClass)].classList.add(`${this.slideClass}--active`);
+    };
 };
 
-const dotsArray = document.querySelectorAll(`.${dotClass}`);
+// --------------------------------------
+// Drag Slider
+class DragSlider {
 
-// Change Class
-dotsArray[0].classList.add(`${dotClass}--active`);
-slideArray[0].classList.add(`${slideClass}--active`);
+    constructor(obj = {}) {
+        this.slideClass = obj.slideClass;
+        this.startCoordX = 0;
+        this.position = 0;
+        this.sliderInner = document.querySelector(`.${this.slideClass}`);
 
-// Check Click
-for (const dot of dotsArray) {
-    dot.addEventListener('click', () => classChange(dot));
-};
-
-function classChange(e) {
-    for (const dot of dotsArray) {
-        dot.classList.remove(`${dotClass}--active`);
+        this.checkDrag();
     };
-    for (const slide of slideArray) {
-        slide.classList.remove(`${slideClass}--active`);
+
+    checkDrag() {
+        this.sliderInner.addEventListener('drag', (e) => {
+            let p = this.position - (this.startCoordX - e.clientX);
+            gsap.to(this.sliderInner, {
+                x: p,
+            });
+        });
+        this.sliderInner.addEventListener('dragstart', (e) => this.startCoordX = e.clientX);
     };
-    e.classList.add(`${dotClass}--active`);
-    slideArray[e.getAttribute(dotClass)].classList.add(`${slideClass}--active`);
+    
 };
