e12dc308b5e8046cac05615d665e6ede9f3b25a4
Alex <nike95125@gmail.com>
2022-08-25 15:17:27 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,67 @@ const mainSlider = new DotSlider({
     dotClass: 'main-slider__dot',
     slideClass: 'main-slider__item',
     dotsInnerClass: 'main-slider__dots-inner',
+    btnClass: 'main-slider__btn',
+    btnClassInner: 'main-slider__btn-inner',
+});
+
+const aboutDotSlider = new DotSlider({
+    dotClass: 'about__dot-slider__dot',
+    slideClass: 'about__dot-slider__item',
+    dotsInnerClass: 'about__dot-slider__inner',
+    btnClass: 'about__dot-slider__btn',
+    btnClassInner: 'about__dot-slider__btn-inner',
 });
 
 const aboutDragSlider = new DragSlider({
     slideClass: 'about__slider',
 });
+
+
+
+// Rest Code
+
+// Optimize Title Text
+if (document.body.offsetWidth < 1400) {
+    changeTitleText();
+};
+
+window.addEventListener('resize', () => {
+    if (document.body.offsetWidth < 1400) {
+        changeTitleText();
+    };
+});
+
+function changeTitleText() {
+    document.querySelectorAll('.title').forEach(title => {
+        title.textContent = title.textContent.trim()
+        if (title.textContent.length > 50) {
+            title.textContent = title.textContent
+                .split('')
+                .splice(0, 46)
+                .join('');
+            title.textContent += '...';
+        };
+    });
+};
+
+$(function () {
+
+    // Burger Menu
+    $('.header-menu__btn').on('click', function () {
+        $('.menu').toggleClass('menu--active');
+        $(this).toggleClass('header-menu__btn-resolve');
+        if ($('.menu').hasClass('menu--active')) {
+            bodyOverFlow(true, document.querySelector(`.${mainSlider.btnClass}--left`), document.querySelector(`.${mainSlider.btnClass}--right`));
+        } else {
+            bodyOverFlow(false, document.querySelector(`.${mainSlider.btnClass}--left`), document.querySelector(`.${mainSlider.btnClass}--right`));
+        }
+    });
+
+});
+
+function bodyOverFlow(param, elem1, elem2) {
+    param ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
+    param ? elem1.style.opacity = '0' : elem1.style.opacity = '1';
+    param ? elem2.style.opacity = '0' : elem2.style.opacity = '1';
+};
--- app/js/slider.js
+++ app/js/slider.js
@@ -6,45 +6,110 @@ class DotSlider {
         this.slideClass = obj.slideClass;
         this.dotsInnerClass = obj.dotsInnerClass;
 
+        obj?.btnClass ? this.btnClass = obj.btnClass : this.btnClass = '';
+        if (obj?.btnClassInner)  {
+            this.btnClassInner = obj.btnClassInner;
+            this.btnInner = document.querySelector(`.${obj.btnClassInner}`);
+        };
+
         this.slideArray = document.querySelectorAll(`.${this.slideClass}`);
         this.dotsInner = document.querySelector(`.${this.dotsInnerClass}`);
-        this.DOTS_COUNT = this.slideArray.length;
+
+        this.slidesCount = this.slideArray.length;
+        this.DOTS_COUNT = this.slidesCount;
+        this.counter = 0;
 
         this.createDots();
+        this.btnClass !== '' ? this.createBtn() : 0;
     };
 
     createDots() {
+
         for (let i = 0; i < this.DOTS_COUNT; i++) {
             const dot = document.createElement('li');
             dot.classList.add(this.dotClass);
             dot.setAttribute(this.dotClass, i);
             this.dotsInner.append(dot);
         };
+
         this.dotsArray = document.querySelectorAll(`.${this.dotClass}`);
         this.firstClassChange();
+
+    };
+
+    createBtn() {
+
+        const btnLeft = document.createElement('button');
+        const btnRight = document.createElement('button');
+
+        btnLeft.classList.add(`${this.btnClass}--left`);
+        btnRight.classList.add(`${this.btnClass}--right`);
+
+        this.btnInner.append(btnLeft);
+        this.btnInner.append(btnRight);
+
+        this.btnL = document.querySelector(`.${this.btnClass}--left`);
+        this.btnR = document.querySelector(`.${this.btnClass}--right`);
+
+        this.btnArray = [];
+        this.btnArray[0] = this.btnL;
+        this.btnArray[1] = this.btnR;
+
+        this.checkClickBtn();
+
     };
 
     firstClassChange() {
+
         this.dotsArray[0].classList.add(`${this.dotClass}--active`);
         this.slideArray[0].classList.add(`${this.slideClass}--active`);
-        this.checkClick();
+        this.checkClickDot();
+
     };
 
-    checkClick() {
+    checkClickDot() {
+
         for (const dot of this.dotsArray) {
             dot.addEventListener('click', () => this.classChange(dot));
         };
+
+    };
+
+    checkClickBtn() {
+
+        this.btnArray[0].addEventListener('click', () => {
+            if (this.counter > 0) {
+                this.counter--;
+                this.classChange('btn');
+            };
+        });
+
+        this.btnArray[1].addEventListener('click', () => {
+            if (this.counter < this.slidesCount - 1) {
+                this.counter++;
+                this.classChange('btn');
+            };
+        });
+
     };
 
     classChange(e) {
-        for (const dot of this.dotsArray) {
-            dot.classList.remove(`${this.dotClass}--active`);
-        };
+
         for (const slide of this.slideArray) {
             slide.classList.remove(`${this.slideClass}--active`);
         };
-        e.classList.add(`${this.dotClass}--active`);
-        this.slideArray[e.getAttribute(this.dotClass)].classList.add(`${this.slideClass}--active`);
+        for (const dot of this.dotsArray) {
+            dot.classList.remove(`${this.dotClass}--active`);
+        };
+        if (e !== 'btn') {
+            e.classList.add(`${this.dotClass}--active`);
+            this.slideArray[e.getAttribute(this.dotClass)].classList.add(`${this.slideClass}--active`);
+            this.counter = e.getAttribute(this.dotClass);
+        } else {
+            this.slideArray[this.counter].classList.add(`${this.slideClass}--active`);
+            this.dotsArray[this.counter].classList.add(`${this.dotClass}--active`);
+        };
+
     };
 };
 
@@ -58,17 +123,28 @@ class DragSlider {
         this.position = 0;
         this.sliderInner = document.querySelector(`.${this.slideClass}`);
 
-        this.checkDrag();
+        this.checkEvent();
     };
 
-    checkDrag() {
+    checkEvent() {
+
         this.sliderInner.addEventListener('drag', (e) => {
-            let p = this.position - (this.startCoordX - e.clientX);
+            changePos(e);
+        });
+
+        this.sliderInner.addEventListener('dragend', (e) => {
+            changePos(e);
+        });
+
+        let changePos = (event) => {
+            let p = this.startCoordX - (this.startCoordX + event.screenX);
             gsap.to(this.sliderInner, {
                 x: p,
             });
-        });
-        this.sliderInner.addEventListener('dragstart', (e) => this.startCoordX = e.clientX);
+        };
+
+        this.sliderInner.addEventListener('dragstart', (e) => this.startCoordX = e.screenX);
+        
     };
-    
+
 };
